aa64336a09d2e0242a37bb60a939699d3769b163
RealDanTheMan <dantheman.py@gmail.com>
2025-03-17 23:07:26 +0000

    JSONChunks can now be serialised to pure JOSN dictionary

--- shadercraft/serialisation.py
+++ shadercraft/serialisation.py
@@ -1,19 +1,58 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
+from .asserts import assertType, assertRef, assertTrue
 
 @dataclass
 class JSONChunk:
     """
+    Chunk represents a arbitrary section of JSON data in dictionary format.
 
     """
     data: dict
     version: int
     classname: str
 
+    @classmethod
+    def toJson(cls, chunk: JSONChunk) -> dict:
+        """
+        Serialises this chunk to JSON like dictionary
+
+        """
+        assertType(chunk, JSONChunk)
+        assertType(chunk.classname, str)
+        assertType(chunk.version, int)
+        assertType(chunk.data, dict)
+
+        data: dict = {
+            "classname" : chunk.classname,
+            "version"   : str(chunk.version),
+            "data"      : chunk.data
+        }
+
+        return data
+
+    @classmethod
+    def fromJson(cls, data: dict) -> JSONChunk:
+        """
+        Dersialise JSON chunk from JSON like dictionary.
+
+        """
+        assertType(data, dict)
+        assertTrue("classname" in data)
+        assertTrue("version" in data)
+        assertTrue("data" in data)
+
+        return JSONChunk(
+            data["data"],
+            int(data["version"]),
+            data["classname"]
+        )
+
 
 class ISerialisableJSON():
     """
+    Interfave class responsible for serialising and deserialising JSON content.
 
     """
 
